210b59140443041fd3a083bf4d10fe1b6a15e36b
annyshkarina <147395933+annyshkarina@users.noreply.github.com>
2026-05-14 11:15:57 +0300

    secrets.py

--- src/vuln_analyzer/secrets.py
+++ src/vuln_analyzer/secrets.py
@@ -4,9 +4,11 @@ def detect_secrets(code: str):
     issues = []
 
     patterns = {
+        "Password": r"(?i)password\s*=\s*['\"].+?['\"]",
         "API Key": r"(?i)api[_-]?key\s*=\s*['\"][A-Za-z0-9_\-]{10,}['\"]",
         "AWS Key": r"AKIA[0-9A-Z]{16}",
-        "Token": r"(?i)token\s*=\s*['\"][A-Za-z0-9_\-]{10,}['\"]"
+        "Token": r"(?i)token\s*=\s*['\"][A-Za-z0-9_\-]{10,}['\"]",
+        "Secret": r"(?i)secret\s*=\s*['\"].+?['\"]",
     }
 
     for name, pattern in patterns.items():
